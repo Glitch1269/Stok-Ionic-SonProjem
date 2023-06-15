5e2ad2385e269cde016d56c721ad0c54d3a517fc
Glitch1269 <111871384+Glitch1269@users.noreply.github.com>
2023-06-15 15:03:29 +0300

    Add files via upload

---
+++ Stok-api/View Model/ItemModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stok_Ionic_Api.View_Model
+{
+    public class ItemModel
+    {
+        public string itemId { get; set; }
+        public string itemKatTur { get; set; }
+        public string itemAdi { get; set; }
+        public int itemMiktar { get; set; }
+        public string itemKayitTarih { get; set; }
+        public string itemDuzenlenmeTarih { get; set; }
+        public string itemPng { get; set; }
+        public string itemResim { get; set; }
+
+    }
+}
